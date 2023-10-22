291b57647d12a052fec95f1607a3406d96731bd4
Jimmy Reyes <jrfullstack@gmail.com>
2023-10-22 21:35:34 +0200

    Nuevos metodos: with, structureClone, to...

---
+++ 13-esnext/main.js
@@ -0,0 +1,9 @@
+import './style.css';
+// import './01-arrays/01-array-with';
+import './01-arrays/03-array-to-methods';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Hola Mundo</h1>
+  </div>
+`
